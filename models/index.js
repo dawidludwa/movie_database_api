1de2c7e552b8f8cdef56d93bc8cf3c4a90a97a5e
Michał Burdzy <mmicalt@gmail.com>
2018-07-03 15:01:34 +0000

    updated db url as env variable

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-// mongoose.connect('mongodb://localhost/movies');
-mongoose.connect('mongodb://mmicalt:Roka1991@ds125241.mlab.com:25241/movies_api_database')
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect('mongodb://mmicalt:Roka1991@ds125241.mlab.com:25241/movies_api_database')
+
 mongoose.Promise = Promise;
 mongoose.set('debug', 'true');
 
