5be798b3a6805eb8c3b8d6f315f6b66e6e1dc685
Michał Burdzy <mmicalt@gmail.com>
2018-07-03 14:45:13 +0000

    added mLab database reference

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/movies')
+// mongoose.connect('mongodb://localhost/movies');
+mongoose.connect('mongodb://mmicalt:Roka1991@ds125241.mlab.com:25241/movies_api_database')
 mongoose.Promise = Promise;
 mongoose.set('debug', 'true');
 
